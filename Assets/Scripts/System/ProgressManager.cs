a0e3e93d84d6928b2bb7c78a725bc0a33fe37c7c
Pallavi <pallavip@usc.edu>
2025-11-06 15:03:18 -0800

    lock level 2 at home page

--- Assets/Scripts/System/ProgressManager.cs
+++ Assets/Scripts/System/ProgressManager.cs
@@ -6,4 +6,18 @@ public static class ProgressManager {
     public static bool IsLevel1Complete()   => PlayerPrefs.GetInt(L1_COMPLETE, 0) == 1;
     public static bool IsLevelUnlocked(int level)
         => level <= 1 || (level == 2 && IsLevel1Complete());
+
+    public static void ResetAllProgress() {
+        PlayerPrefs.DeleteKey(L1_COMPLETE);
+        PlayerPrefs.Save();
+    }
+
+#if UNITY_EDITOR
+    static bool editorAutoResetApplied;
+    public static void TryAutoResetInEditorSession() {
+        if (editorAutoResetApplied) return;
+        editorAutoResetApplied = true;
+        ResetAllProgress();
+    }
+#endif
 }
--- Assets/Scripts/UI/HomeLevelButtons.cs
+++ Assets/Scripts/UI/HomeLevelButtons.cs
@@ -4,10 +4,25 @@ using UnityEngine.UI;
 public class HomeLevelButtons : MonoBehaviour {
     public Button level1Btn, level2Btn;
     public GameObject level2LockIcon;
+#if UNITY_EDITOR
+    [SerializeField] bool resetProgressEachPlayInEditor = true;
+#endif
 
-    void Start() {
-        bool l2 = ProgressManager.IsLevelUnlocked(2);
-        level2Btn.interactable = l2;
-        if (level2LockIcon) level2LockIcon.SetActive(!l2);
+    void Awake() {
+#if UNITY_EDITOR
+        if (Application.isPlaying && resetProgressEachPlayInEditor)
+            ProgressManager.TryAutoResetInEditorSession();
+#endif
+    }
+
+    void OnEnable() => RefreshLevelLocks();
+    void Start() => RefreshLevelLocks();
+
+    void RefreshLevelLocks() {
+        if (!level2Btn) return;
+
+        bool level2Unlocked = ProgressManager.IsLevelUnlocked(2);
+        level2Btn.interactable = level2Unlocked;
+        if (level2LockIcon) level2LockIcon.SetActive(!level2Unlocked);
     }
 }
