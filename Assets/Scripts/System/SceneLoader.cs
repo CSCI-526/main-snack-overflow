6dd90d75dea1154ac957fea10520ce113b27d56c
Mihika Dakappagari <dakappag@usc.edu>
2025-11-06 20:02:14 -0800

    Improved UI

--- Assets/Scripts/System/SceneLoader.cs
+++ Assets/Scripts/System/SceneLoader.cs
@@ -14,11 +14,18 @@ public class SceneLoader : MonoBehaviour {
         GameMode.SetTutorial(false);
         SceneManager.LoadScene("LvL1");
     }
-    public void LoadLevel2() {
-        if (!ProgressManager.IsLevelUnlocked(2)) {
+    public void LoadLevel2()
+    {
+        if (!ProgressManager.IsLevelUnlocked(2))
+        {
             Debug.LogWarning("Tried to load Level 2 before Level 1 was complete.");
             return;
         }
         SceneManager.LoadScene("LvL2");
+    }
+    
+    public void LoadHome() {
+        GameMode.SetTutorial(false);
+        SceneManager.LoadScene("Home", LoadSceneMode.Single);
     }
 }
